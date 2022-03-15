0b3b4d49e0652ced792bc7a54b83986bad070766
Alana Muller <amuller@email.arizona.edu>
2022-03-15 10:27:36 -0700

    Add outline for data simulation

---
+++ simulate-plot-data.R
@@ -0,0 +1,11 @@
+# Simulate and plot data
+# Alana Muller
+# amuller@email.arizona.edu
+# 2022-03-15
+
+# Simulate predictor variable
+
+# Calculate response variable and add noise
+
+# Plot the data
+
